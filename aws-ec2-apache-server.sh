0dafe8c55a61a712e10c6e50827adeb4418a4aad
Shakir Masaari <35549045+shakir85@users.noreply.github.com>
2020-07-18 17:03:34 -0500

    Comment on last line

--- aws-ec2-apache-server.sh
+++ aws-ec2-apache-server.sh
@@ -11,4 +11,5 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
+# Echo for testing http server
 echo "Hello world from $(hostname -f)" > /var/www/html/index.html
